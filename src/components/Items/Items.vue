c45ca278d1d8484dd3b6892b3405ade927b9a034
Mahmudul Hasan Hasib <hasib.mahmudul@gmail.com>
2022-05-15 19:14:32 +0600

    add stock quantity

--- src/components/Items/Items.vue
+++ src/components/Items/Items.vue
@@ -17,30 +17,35 @@ export default {
                     id: 1,
                     name: "Bashmoti Rice",
                     price: 100.00,
+                    stockQuantity: 250,
                     image: './../../assets/img/grocery-items.png'
                 },
                 {
                     id: 2,
                     name: "Fresh Pineapple",
                     price: 300.00,
+                    stockQuantity: 990,
                     image: './../../assets/img/fresh-fruit.png'
                 },
                 {
                     id: 3,
                     name: "Another Rice",
                     price: 200.00,
+                    stockQuantity: 500,
                     image: './../../assets/img/grocery-items.png'
                 },
                 {
                     id: 4,
                     name: "Another Fruit",
                     price: 150.00,
+                    stockQuantity: 200,
                     image: './../../assets/img/fresh-fruit.png'
                 },
                 {
                     id: 5,
                     name: "Another Fruit 2",
                     price: 160.00,
+                    stockQuantity: 350,
                     image: './../../assets/img/fresh-fruit.png'
                 },
             ],
--- src/components/Items/SingleItem.vue
+++ src/components/Items/SingleItem.vue
@@ -6,6 +6,7 @@
         <div class="product-details text-center mb-5">
             <h4 class="text-2xl">{{ item.name }}</h4>
             <h3 class="text-xl"><strong>Price: </strong> {{ item.price }}</h3>
+            <h3 class="text-xl"><strong>Stock: </strong> {{ item.stockQuantity }}</h3>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-5 rounded shadow-2xl"
                 @click="addToCart(item)">
                 Add To Cart
@@ -21,6 +22,7 @@ export default {
     },
     methods: {
         addToCart(item) {
+            this.item.stockQuantity -= 1;
             this.eventBus.emit('addItemToCart', item);
         }
     },
